954e6c22d1d2b951eed6b09bd97c206dc838ae9a
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-18 18:27:03 -0300

    feat(ports): Add GenerateGlossary to LLMProvider interface

--- internal/ports/translator.go
+++ internal/ports/translator.go
@@ -1,5 +1,8 @@
 package ports
 
+import "translator-worker/internal/domain"
+
 type LLMProvider interface {
 	TranslateText(text string, src, tgt string) (string, error)
+	GenerateGlossary(text string, sourceLang, targetLang string) ([]domain.GlossaryItem, error)
 }
